66fc5438a13bc66d8a1cabc8d06f680d5213516f
VamshiRajRj <v2m431raj@gmail.com>
2024-03-06 23:42:34 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,424 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="generator" content="Hugo 0.48" />
+    <meta charset="utf-8" />
+    <meta name="robots" content="index, follow" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Dev Space by Lapa Ninja</title>
+    <meta name="keywords" content="yeo" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:200,400,700"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="./css/cards.css" />
+    <link rel="stylesheet" href="./css/spectre.css" />
+    <link rel="stylesheet" href="./css/spectre-icons.css" />
+    <link rel="stylesheet" href="./css/spectre-exp.css" />
+    <link rel="stylesheet" href="./css/yeo.css" />
+
+    <meta property="og:title" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:type" content="product" />
+    <meta property="og:image" content="" />
+  </head>
+
+  <body>
+    <div class="yeo-slogan">
+      <div class="container yeo-header">
+        <div class="columns">
+          <div class="column col-12">
+            <header class="navbar">
+              <section class="navbar-section">
+                <a class="navbar-brand logo" href="./">
+                  <img class="logo-img" src="./images/logo.svg" alt="" /><span
+                    >App Maker</span
+                  >
+                </a>
+              </section>
+              <section class="navbar-section hide-sm">
+                <a class="btn btn-link" href="#we-do">What we do</a>
+                <a class="btn btn-link" href="#we-work">How we work</a>
+                <a class="btn btn-link" href="#team">Our Team</a>
+                <a class="btn btn-link" href="#news">News</a>
+              </section>
+            </header>
+          </div>
+        </div>
+      </div>
+      <div class="container slogan">
+        <div class="columns">
+          <div class="column col-7 col-sm-12">
+            <div class="slogan-content">
+              <h1>
+                <span class="slogan-bold">App Design</span>
+                <span class="slogan-bold">App Development</span>
+                <span class="slogan-bold">Consultant Services</span>
+              </h1>
+              <p>We help you turn your ideas into digital realities</p>
+              <!-- <a
+                class="btn btn-primary btn-lg btn-start"
+                target="_blank"
+                href="#y"
+                >Start a project today</a
+              > -->
+            </div>
+          </div>
+          <div class="column col-5 hide-sm">
+            <img class="slogan-img" src="./images/yeo-feature-1.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="yeo-client">
+      <div class="container yeo-client-list">
+        <div class="columns">
+          <div class="column col-12">
+            <h3 class="feature-title">Our Partners</h3>
+            <div class="client-logo">
+              <a href="#" target="_blank">
+                <img src="./images/algolia.svg" height="40px" alt="" />
+              </a>
+              <a href="#" target="_blank">
+                <img src="./images/lapa-logo.svg" height="40px" alt="" />
+              </a>
+              <a href="#" target="_blank">
+                <img src="./images/hugo.svg" height="40px" alt="" />
+              </a>
+              <a href="#" target="_blank">
+                <img src="./images/codecademy.svg" height="40px" alt="" />
+              </a>
+              <a href="#" target="_blank">
+                <img src="./images/aws.svg" height="40px" alt="" />
+              </a>
+              <a href="#" target="_blank">
+                <img src="./images/stripe.svg" height="40px" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="yeo-do" id="we-do">
+      <div class="container yeo-body">
+        <div class="columns">
+          <div class="column col-12">
+            <h2 class="feature-title">What We Do</h2>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <div class="yeo-do-content">
+              <img src="./images/what-we-do-1.svg" alt="" />
+              <h3>Interface Design</h3>
+              <p>We convert your idea into visually appealing design</p>
+            </div>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <div class="yeo-do-content">
+              <img src="./images/what-we-do-2.svg" alt="" />
+              <h3>App Development</h3>
+              <p>We turn the design into usable mobile applications</p>
+            </div>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <div class="yeo-do-content">
+              <img src="./images/what-we-do-3.svg" alt="" />
+              <h3>Business Plan design</h3>
+              <p>
+                We turn your idea into a business model and help you generate
+                revenue.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="yeo-work" id="we-work">
+      <div class="container yeo-body">
+        <div class="columns">
+          <div class="column col-12 col-sm-12">
+            <h2 class="feature-title">How We Work</h2>
+          </div>
+          <div class="column col-10 col-sm-12 centered">
+            <h2 class="yeo-work-feature">
+              We build
+              <span class="yeo-work-feature-bold">reliable infrastructure</span
+              >, use competetive in-house staff.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="yeo-price" id="price">
+      <div class="container yeo-body col-sm-12">
+        <div class="columns">
+          <div class="column col-4 col-sm-12">
+            <div class="panel">
+              <div class="panel-header">
+                <div class="panel-title"><span>PRO</span></div>
+              </div>
+              <div class="panel-body">
+                <p>
+                  <span class="price">$2500 </span>
+                  <span>/ month</span>
+                </p>
+                <ul>
+                  <li>
+                    <strong>50</strong> billable hours included per month.
+                  </li>
+                  <li>
+                    Billable hourly rate of <strong>$30</strong> after the
+                    included 50 hours are utilized.
+                  </li>
+                  <li>24/7/365 on-call and 99.9% SLA</li>
+                  <li>Communication via ticketing system and e-mail.</li>
+                </ul>
+              </div>
+              <div class="panel-footer">
+                <button class="btn btn-primary btn-price">Contact Us</button>
+              </div>
+            </div>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <div class="panel">
+              <div class="panel-header">
+                <div class="panel-title"><span>PREMIUM</span></div>
+              </div>
+              <div class="panel-body">
+                <p>
+                  <span class="price">$4000 </span>
+                  <span>/ month</span>
+                </p>
+                <ul>
+                  <li>
+                    <strong>80</strong> billable hours included per month.
+                  </li>
+                  <li>
+                    Billable hourly rate of <strong>$25</strong> after the
+                    included 80 hours are utilized.
+                  </li>
+                  <li>24/7/365 on-call and 99.9% SLA</li>
+                  <li>Communication via ticketing system and e-mail.</li>
+                </ul>
+              </div>
+              <div class="panel-footer">
+                <button class="btn btn-primary btn-price">Contact Us</button>
+              </div>
+            </div>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <div class="panel">
+              <div class="panel-header">
+                <div class="panel-title"><span>ENTERPRISE</span></div>
+              </div>
+              <div class="panel-body">
+                <p>
+                  <span class="price">$8000 </span>
+                  <span>/ month</span>
+                </p>
+                <ul>
+                  <li>
+                    <strong>120</strong> billable hours included per month.
+                  </li>
+                  <li>
+                    Billable hourly rate of <strong>$20</strong> after the
+                    included 120 hours are utilized.
+                  </li>
+                  <li>24/7/365 on-call and 99.9% SLA</li>
+                  <li>Communication via ticketing system and e-mail.</li>
+                </ul>
+              </div>
+              <div class="panel-footer">
+                <button class="btn btn-primary btn-price">Contact Us</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="yeo-team" id="team">
+      <div class="container yeo-body">
+        <div class="columns">
+          <div class="column col-12">
+            <h2 class="feature-title">Meet Our Team</h2>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <a href=""
+              ><img
+                class="s-circle"
+                src="https://t3.ftcdn.net/jpg/06/01/17/18/360_F_601171862_l7yZ0wujj8o2SowiKTUsfLEEx8KunYNd.jpg"
+                alt=""
+            /></a>
+            <a href="https://www.lapa.ninja/"
+              ><span class="name">Vamshi</span></a
+            >
+            <span class="title">DevOps</span>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <a href=""
+              ><img
+                class="s-circle"
+                src="https://t3.ftcdn.net/jpg/06/01/17/18/360_F_601171867_X85WpWCcMzNsoMWtMxiZQspKzaOwCyuK.jpg"
+                alt=""
+            /></a>
+            <a href="https://www.lapa.ninja/"
+              ><span class="name">Tinh Nguyen</span></a
+            >
+            <span class="title">Designer</span>
+          </div>
+          <div class="column col-4 col-sm-12">
+            <a href=""
+              ><img
+                class="s-circle"
+                src="https://img.freepik.com/premium-photo/portrait-successful-programmer-game-developer-coder-guy-uses-computer-laptop-work-game-design-hacker-boy-generative-ai_117038-5477.jpg"
+                alt=""
+            /></a>
+            <a href="https://www.lapa.ninja/"><span class="name">Raj</span></a>
+            <span class="title">Developer</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="yeo-open-source">
+      <div class="container yeo-body">
+        <div class="columns">
+          <div class="column col-12">
+            <h2 class="feature-title">We love Open Source</h2>
+          </div>
+          <div class="column col-10 centered col-sm-12">
+            <img
+              class="open-source-img"
+              src="./images/open-source.svg"
+              alt=""
+            />
+            <h2 class="open-source-feature">
+              We're making
+              <br />
+              Open Source Software
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--news section-->
+    <section id="news" class="py-7 bg-white section-angle top-left bottom-left">
+      <div class="containerr">
+        <div class="roww">
+          <div class="col-md-6 mx-auto text-center">
+            <h2 class="heading-black">News from Knight.</h2>
+            <p class="text-muted lead">What's new at Knight.</p>
+          </div>
+        </div>
+        <div class="roww mt-5">
+          <div class="col-md-4">
+            <div class="card">
+              <a href="#">
+                <img
+                  class="card-img-top img-raised"
+                  src="images/blog-1.jpg"
+                  alt="Blog 1"
+                />
+              </a>
+              <div class="card-body">
+                <a href="#" class="card-title mb-2"
+                  ><h5>We launch new iOS & Android mobile apps</h5></a
+                >
+                <p class="text-muted small-xl mb-2">Sep 27, 2018</p>
+                <p class="card-text">
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imper, consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <a href="#">
+                <img
+                  class="card-img-top img-raised"
+                  src="images/blog-2.jpg"
+                  alt="Blog 2"
+                />
+              </a>
+              <div class="card-body">
+                <a href="#" class="card-title mb-2"
+                  ><h5>New update is available for the editor</h5></a
+                >
+                <p class="text-muted small-xl mb-2">August 16, 2018</p>
+                <p class="card-text">
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imper, consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <a href="#">
+                <img
+                  class="card-img-top img-raised"
+                  src="images/blog-3.jpg"
+                  alt="Blog 3"
+                />
+              </a>
+              <div class="card-body">
+                <a href="#" class="card-title mb-2"
+                  ><h5>The story of building #1 page builder</h5></a
+                >
+                <p class="text-muted small-xl mb-2">December 2nd, 2017</p>
+                <p class="card-text">
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imper, consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="yeo-footer">
+      <div class="container">
+        <div class="columns">
+          <div class="column col-3 col-sm-6">
+            <div class="yeo-footer-content">
+              <h4>Clients</h4>
+              <ul class="nav">
+                <li class="nav-item">
+                  <a href="">Lapa Ninja</a>
+                </li>
+                <li class="nav-item">
+                  <a href="">Noty IM</a>
+                </li>
+                <li class="nav-item">
+                  <a href="">Yeo Space</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="column col-3 col-sm-6">
+            <div class="yeo-footer-content">
+              <h4>Contact Us</h4>
+              <ul class="nav">
+                <li class="nav-item">
+                <a href=''><?php echo explode(";",file_get_contents( "contacts" ))[0] ?></a>
+                </li>
+                <li class="nav-item">
+                <a href=''><?php echo explode(";",file_get_contents( "contacts" ))[1] ?></a>
+                </li>
+                <li class="nav-item">
+                <a href=''><?php echo explode(";",file_get_contents( "contacts" ))[2] ?></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
